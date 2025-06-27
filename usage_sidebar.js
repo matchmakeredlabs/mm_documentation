7db4fe359e20cb548935a6e31d7edc3ebe27e961
Yonatan Ahituv <ahituvyonatan@gmail.com>
2025-06-26 20:47:18 -0700

    Finish complete outline

--- usage_sidebar.js
+++ usage_sidebar.js
@@ -109,6 +109,65 @@ const sidebars = {
           id: "use_cases/intro",
           label: "Introduction",
         },
+        {
+          type: "category",
+          label: "Transfer Students with MatchMaker",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "use_cases/uni/intro",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "use_cases/uni/intro",
+              label: "Introduction",
+            },
+            {
+              type: "doc",
+              id: "use_cases/uni/collections",
+              label: "Moving Courses into MatchMaker",
+            },
+            {
+              type: "doc",
+              id: "use_cases/uni/matching",
+              label: "Matching Transfer Courses",
+            },
+            {
+              type: "doc",
+              id: "use_cases/uni/conclusion",
+              label: "Conclusion",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Career Change with MatchMaker",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "use_cases/career_change/intro",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "use_cases/career_change/intro",
+              label: "Introduction",
+            },
+            {
+              type: "doc",
+              id: "use_cases/career_change/matching",
+              label: "Matching Existing Knowledge with New Requirements",
+            },
+            {
+              type: "doc",
+              id: "use_cases/career_change/conclusion",
+              label: "Conclusion",
+            },
+          ],
+        },
       ],
     },
     {
@@ -126,12 +185,142 @@ const sidebars = {
           id: "vocab/intro",
           label: "Introduction",
         },
+        {
+          type: "doc",
+          id: "vocab/login",
+          label: "Login",
+        },
+        {
+          type: "category",
+          label: "Collection Management",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "vocab/collection_management/intro",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "vocab/collection_management/display",
+              label: "Display",
+            },
+            {
+              type: "doc",
+              id: "vocab/collection_management/create",
+              label: "Create",
+            },
+            {
+              type: "doc",
+              id: "vocab/collection_management/edit",
+              label: "Edit",
+            },
+            {
+              type: "doc",
+              id: "vocab/collection_management/import_export",
+              label: "Import and Export",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Describing Elements",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "vocab/describe/intro",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "vocab/describe/intro",
+              label: "Introduction",
+            },
+            {
+              type: "doc",
+              id: "vocab/describe/palet",
+              label: "The Palet",
+            },
+            {
+              type: "doc",
+              id: "vocab/describe/describe",
+              label: "Describe Mode",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Report Generation",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "vocab/report_gen/intro",
+          },
+          items: [
+            {
+              type: "category",
+              label: "Matching Sets",
+              collapsible: true,
+              collapsed: true,
+              link: {
+                type: "doc",
+                id: "vocab/report_gen/match_sets/intro",
+              },
+              items: [
+                {
+                  type: "doc",
+                  id: "vocab/report_gen/match_sets/selection",
+                  label: "Selection of Sets",
+                },
+                {
+                  type: "doc",
+                  id: "vocab/report_gen/match_sets/weighting",
+                  label: "Weighting of Matches",
+                },
+                {
+                  type: "doc",
+                  id: "vocab/report_gen/match_sets/results",
+                  label: "Processing Results",
+                },
+              ],
+            },
+            {
+              type: "doc",
+              id: "vocab/report_gen/custom_sets",
+              label: "Custom Sets",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Administration",
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: "doc",
+            id: "vocab/admin/intro",
+          },
+          items: [
+            {
+              type: "doc",
+              id: "vocab/admin/organizations",
+              label: "Organizations",
+            },
+            {
+              type: "doc",
+              id: "vocab/admin/groups",
+              label: "Groups",
+            },
+          ],
+        },
       ],
     },
     {
       type: "doc",
       label: "MatchMaker FAQ",
-      id: "faq/intro",
+      id: "faq",
     },
 
     // {
