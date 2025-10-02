9c7055bd55436cbe61663f9dbea72af80918cb9d
Yonatan Ahituv <ahituvyonatan@gmail.com>
2025-10-02 11:38:45 -0700

    Update Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
+        <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/usage/starting/intro"
